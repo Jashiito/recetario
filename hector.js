27ee9dbe0c324bbfeca60b9a4289a0dce4e9ad8f
Hector Sandoval <jhondlc29@gmail.com>
2025-05-16 15:41:54 -0500

    mejorado

--- hector.js
+++ hector.js
@@ -1,10 +1,11 @@
 function guardar(){
-    var Titulo = document.getElementById("Titulo").value;
-    var Ingredientes = document.getElementById("Ingredientes").value;
-    var Pasos = document.getElementById("Paso").value;
+    var titulo = document.getElementById("Titulo").value;
+    var ingredientes = document.getElementById("Ingredientes").value;
+    var pasos = document.getElementById("Paso").value;
 
     var taskList = document.getElementById("taskList");
     var row = document.createElement("tr");
-    row.innerHTML = "<td>" + Tituloitulo + "</td><td>" + Ingredientes + "</td><td>" + Pasos + "</td>";
+    row.innerHTML = "<td>" + titulo + "</td><td>" + ingredientes + "</td><td>" + pasos + "</td>";
     taskList.appendChild(row);
-}
+
+}  
